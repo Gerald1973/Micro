ac28d483ece20bd0c6e9bbfab727f2abcd78e702
Gérald Maréchal <gerald1973@gmail.com>
2021-01-22 19:18:25 +0100

    Threading ongoing...

--- src/Micro.cpp
+++ src/Micro.cpp
@@ -11,39 +11,41 @@
 #include <SDL2/SDL.h>
 #include <string>
 #include "GlobalConstants.h"
+#include "Timer.h"
 
 using namespace std;
 
 int main(int argv, char **args) {
+	bool cap = true;
+	const int FREQUENCY = 22050;
+	const int FRAMES_PER_SECOND = 50;
+	const int samples = FREQUENCY / FRAMES_PER_SECOND;
 	SDL_AudioSpec desired, obtained;
 	SDL_zero(obtained);
 	SDL_zero(desired);
-	desired.freq = 44100;
+	desired.freq = FREQUENCY;
 	desired.format = AUDIO_S32SYS;
 	desired.channels = 1;
-	desired.samples = 4096;
-	desired.callback = NULL;
-	string deviceName = "";
-	int choice = 0;
+	desired.samples = samples;
+	desired.callback = nullptr;
+	Timer timer;
 	if (InitUtils::getInstance()->init()) {
-		InitUtils::getInstance()->displayInfoInputSound();
-		cout << "Enter the desired capture device :";
-		cin >> choice;
-		deviceName = SDL_GetAudioDeviceName(choice, 1);
-		cout << "DEBUG: choice: " << choice << endl;
-		cout << "The selected device is : " << deviceName << endl;
-		int deviceIdIn = SDL_OpenAudioDevice(deviceName.c_str(), SDL_TRUE, &desired, &obtained, 0);
+
+		int deviceIdOut = SDL_OpenAudioDevice(nullptr, SDL_FALSE, &desired, &obtained, 0);
+		int deviceIdIn = SDL_OpenAudioDevice(nullptr, SDL_TRUE, &desired, &obtained, 0);
 		if (deviceIdIn == 0) {
-			cout << "ERROR: Can not open the device : " << deviceName.c_str() << endl;
+			cout << "ERROR: Can not open input sound device." << endl;
 		} else {
 			SDL_Window *pWindow = SDL_CreateWindow("Micro", 0, 0, GlobalConstants::SCREEN_WIDTH, GlobalConstants::SCREEN_HEIGHT,
 					SDL_WINDOW_OPENGL | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-			SDL_Renderer *renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC);
+			SDL_Renderer *renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 			SDL_Event event;
 			bool loop = true;
+			float frame = 0;
+
 			while (loop) {
-				SDL_PauseAudioDevice(deviceIdIn, SDL_FALSE);
+				timer.start();
 				while (SDL_PollEvent(&event)) {
 					switch (event.type) {
 					case SDL_KEYDOWN:
@@ -52,17 +54,27 @@ int main(int argv, char **args) {
 						break;
 					}
 				}
-				int buffer[1024];
-				const Uint32 bytesDequeued = SDL_DequeueAudio(deviceIdIn, buffer, sizeof(buffer));
+				int buffer[samples];
+				SDL_PauseAudioDevice(deviceIdOut, SDL_FALSE);
+				SDL_PauseAudioDevice(deviceIdIn, SDL_FALSE);
+				const Uint32 dequeued = SDL_DequeueAudio(deviceIdIn, buffer, sizeof(buffer)) / 4;
+				SDL_QueueAudio(deviceIdOut, buffer, dequeued);
 				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 				SDL_RenderClear(renderer);
 				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-				for (Uint32 c = 0; c < bytesDequeued / 4; c++) {
-					float y = ( buffer[c] / (float) INT32_MAX);
-					y = y * ((float) GlobalConstants::SCREEN_HEIGHT / 2.0);
-					SDL_RenderDrawPoint(renderer, c, y + GlobalConstants::SCREEN_HEIGHT / 2.0);
+				float y = 0;
+				float x = 0;
+				const float halfScreen = (float) GlobalConstants::SCREEN_HEIGHT / 2.0;
+				for (Uint32 c = 0; c < dequeued; c++) {
+					x = ((float) c / (float) dequeued) * (float) GlobalConstants::SCREEN_WIDTH;
+					y = ((float) buffer[c] / (float) INT32_MAX) * halfScreen;
+					SDL_RenderDrawPointF(renderer, x, y + halfScreen);
 				}
 				SDL_RenderPresent(renderer);
+				frame++;
+				if ((cap == true) && (timer.get_ticks() < 1000 / FRAMES_PER_SECOND)) {
+					SDL_Delay((1000 / FRAMES_PER_SECOND) - timer.get_ticks());
+				}
 			}
 			SDL_CloseAudioDevice(deviceIdIn);
 			SDL_Quit();
---
+++ src/Timer.h
@@ -0,0 +1,42 @@
+/*
+ * Timer.h
+ *
+ *  Created on: 21 janv. 2021
+ *      Author: Papa
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+class Timer {
+public:
+private:
+    //The clock time when the timer started
+    int startTicks;
+
+    //The ticks stored when the timer was paused
+    int pausedTicks;
+
+    //The timer status
+    bool paused;
+    bool started;
+
+    public:
+    //Initializes variables
+    Timer();
+
+    //The various clock actions
+    void start();
+    void stop();
+    void pause();
+    void unpause();
+
+    //Gets the timer's time
+    int get_ticks();
+
+    //Checks the status of the timer
+    bool is_started();
+    bool is_paused();
+};
+
+#endif /* TIMER_H_ */
