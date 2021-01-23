82d12e5a5626c5274e310d7be65d6a64ccdfa21d
Gérald Maréchal <gerald1973@gmail.com>
2021-01-23 01:19:10 +0100

    to deploy

--- src/Micro.cpp
+++ src/Micro.cpp
@@ -47,6 +47,18 @@ static int threadFunction(void *data) {
 	return 0;
 }
 
+int toggleFullScreen(SDL_Window *pWindow) {
+	bool result = true;
+	Uint32 fullscreenFlag = SDL_WINDOW_FULLSCREEN_DESKTOP;
+	bool isFullscreen = SDL_GetWindowFlags(pWindow) & fullscreenFlag;
+	int tmp = SDL_SetWindowFullscreen(pWindow, isFullscreen ? 0 : fullscreenFlag);
+	if (tmp < 0) {
+		std::cout << "Error : fullscreen not possible" << std::endl;
+		result = false;
+	}
+	return result;
+}
+
 int main(int argv, char **args) {
 	bool cap = true;
 	Timer timer;
@@ -66,9 +78,14 @@ int main(int argv, char **args) {
 			while (SDL_PollEvent(&event)) {
 				switch (event.type) {
 				case SDL_KEYDOWN:
-				case SDLK_ESCAPE:
-					loop = false;
-					break;
+					switch (event.key.keysym.sym) {
+						case SDLK_F11:
+									toggleFullScreen(pWindow);
+									break;
+						case SDLK_ESCAPE:
+							loop = false;
+							break;
+					}
 				}
 			}
 			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -76,9 +93,13 @@ int main(int argv, char **args) {
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
 			float y = 0;
 			float x = 0;
-			const float halfScreen = (float) GlobalConstants::SCREEN_HEIGHT / 2.0;
+
+			int w = 0;
+			int h = 0;
+			SDL_GetWindowSize(pWindow, &w, &h);
+			const float halfScreen = (float) h / 2.0;
 			for (Uint32 c = 0; c < dequeued/4; c++) {
-				x = ((float) c / ((float) dequeued / 4.0)) * (float) GlobalConstants::SCREEN_WIDTH;
+				x = ((float) c / ((float) dequeued / 4.0)) * (float) w;
 				y = ((float) buffer[c] / (float) INT32_MAX) * halfScreen;
 				SDL_RenderDrawPointF(renderer, x, y + halfScreen);
 			}
